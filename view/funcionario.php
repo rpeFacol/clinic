a9682e962a055bb231b59412ce76f29af4d46507
Alisson Ferreira <alisson.advir@gmail.com>
2017-10-16 22:21:34 -0300

    style para cadastro de funcionario

---
+++ view/funcionario.php
@@ -0,0 +1,23 @@
+<?php 
+    require 'painel.php';
+?>
+<div class="centro">
+<button type="submit" class="btn btn-success" onclick="window.location.href='cadastroFuncionario.php'">
+<span class="glyphicon glyphicon-plus"></span>
+	Cadastrar
+</button>
+
+
+<button type="submit" class="btn btn-primary">
+<span class="glyphicon glyphicon-pencil"></span>
+	Editar
+</button>
+
+<button type="submit" class="btn btn-danger">
+<span class="glyphicon glyphicon-trash"></span>
+	Remover
+</button>
+</div>
+
+</body>
+</html>
